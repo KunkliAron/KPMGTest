90b28089be20aae442f6ccbe82806cb9f08f6a08
Kunkli Áron <47295566+KunkliAron@users.noreply.github.com>
2021-01-29 11:29:36 +0100

    Add files via upload

---
+++ MNBDataLoad/MNBCurrencies.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace mndDataLoad
+{
+	[XmlRoot(ElementName = "Currencies")]
+	public class Currencies
+	{
+		[XmlElement(ElementName = "Curr")]
+		public List<string> Curr { get; set; }
+	}
+
+	[XmlRoot(ElementName = "MNBCurrencies")]
+	public class MNBCurrencies
+	{
+		[XmlElement(ElementName = "Currencies")]
+		public Currencies Currencies { get; set; }
+	}
+}
---
+++ MNBDataLoad/MNBExchangeRates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace exchangeRates
+{
+	[XmlRoot(ElementName = "Rate")]
+	public class Rate
+	{
+		[XmlAttribute(AttributeName = "curr")]
+		public string Curr { get; set; }
+		[XmlAttribute(AttributeName = "unit")]
+		public string Unit { get; set; }
+		[XmlText]
+		public string Text { get; set; }
+	}
+
+	[XmlRoot(ElementName = "Day")]
+	public class Day
+	{
+		[XmlElement(ElementName = "Rate")]
+		public List<Rate> Rate { get; set; }
+		[XmlAttribute(AttributeName = "date")]
+		public string Date { get; set; }
+	}
+
+	[XmlRoot(ElementName = "MNBExchangeRates")]
+	public class MNBExchangeRates
+	{
+		[XmlElement(ElementName = "Day")]
+		public List<Day> Day { get; set; }
+	}
+
+}
